9c19c20aae8495f83a496d226587b7643c26fb1e
Evan Pezent <epezent@rice.edu>
2020-05-09 09:09:30 -0500

    Update implot_demo.cpp

--- implot_demo.cpp
+++ implot_demo.cpp
@@ -21,6 +21,7 @@
 // SOFTWARE.
 
 // ImPlot v0.1 WIP
+
 #ifdef _MSC_VER
 #pragma warning (disable: 4996) // 'This function or variable may be unsafe': strcpy, strdup, sprintf, vsnprintf, sscanf, fopen
 #endif
