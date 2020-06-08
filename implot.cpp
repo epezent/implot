689f1e1b19530b1b163cfafd15a64f47c96748ff
ozlb <ozlb@users.noreply.github.com>
2020-06-08 06:28:05 +0200

    ConstrainNan(ConstrainInf(itemData2.y));

--- implot.cpp
+++ implot.cpp
@@ -3120,7 +3120,7 @@ inline void PlotDigitalEx(const char* label_id, Getter getter, int count, int of
             ImPlotPoint itemData2 = getter(i2);
             i1 = i2;
             if (NanOrInf(itemData1.y)) continue;
-            if (NanOrInf(itemData2.y)) itemData2.y = ConstrainNan(itemData2.y);
+            if (NanOrInf(itemData2.y)) itemData2.y = ConstrainNan(ConstrainInf(itemData2.y));
             int pixY_0 = (int)(line_weight);
             itemData1.y = itemData1.y < 0 ? 0 : itemData1.y;
             float pixY_1_float = gp.Style.DigitalBitHeight * (float)itemData1.y;
