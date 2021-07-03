8f1655b97510e68a85ae24e8ad87a642760d0639
Evan Pezent <epezent16@gmail.com>
2021-07-03 14:30:29 -0700

    set heatmap tex back to GL_NEAREST...oops!

--- backends/implot_impl_opengl3.cpp
+++ backends/implot_impl_opengl3.cpp
@@ -61,8 +61,8 @@ struct HeatmapData
 	HeatmapData() {
 		glGenTextures(1, &HeatmapTexID);
 		glBindTexture(GL_TEXTURE_2D, HeatmapTexID);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 		glBindTexture(GL_TEXTURE_2D, 0);
 	}
 };
