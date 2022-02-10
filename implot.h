a759a9599a88a287ad54adaf2e6f4ce7fcdc4c12
Evan Pezent <epezent16@gmail.com>
2022-02-09 18:56:57 -0800

    buffer

--- implot.h
+++ implot.h
@@ -250,9 +250,9 @@ enum ImPlotBarsFlags_ {
 
 // Flags for PlotBarGroups
 enum ImPlotBarGroupsFlags_ {
-    ImPlotBarGroupsFlags_None       = 0,       // default
-    ImPlotBarGroupsFlags_Horizontal = 1 << 10, // bar groups will be rendered horizontally on the current y-axis
-    ImPlotBarGroupsFlags_Stacked    = 1 << 11, // items in a group will be stacked on top of each other
+    ImPlotBarGroupsFlags_None        = 0,       // default
+    ImPlotBarGroupsFlags_Horizontal  = 1 << 10, // bar groups will be rendered horizontally on the current y-axis
+    ImPlotBarGroupsFlags_Stacked     = 1 << 11, // items in a group will be stacked on top of each other
 };
 
 // Flags for PlotErrorBars
@@ -808,6 +808,8 @@ IMPLOT_API void SetNextAxesToFit();
 IMPLOT_TMP void PlotLine(const char* label_id, const T* values, int count, double xscale=1, double x0=0, ImPlotLineFlags flags=0, int offset=0, int stride=sizeof(T));
 IMPLOT_TMP void PlotLine(const char* label_id, const T* xs, const T* ys, int count, ImPlotLineFlags flags=0, int offset=0, int stride=sizeof(T));
 IMPLOT_API void PlotLineG(const char* label_id, ImPlotGetter getter, void* data, int count, ImPlotLineFlags flags=0);
+
+void PlotLineStagedInternal(const char *label_id, const double *xs, const double *ys, int count);
 
 // Plots a standard 2D scatter plot. Default marker is ImPlotMarker_Circle.
 IMPLOT_TMP void PlotScatter(const char* label_id, const T* values, int count, double xscale=1, double x0=0, ImPlotScatterFlags flags=0, int offset=0, int stride=sizeof(T));
