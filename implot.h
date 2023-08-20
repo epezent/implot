2c71f5815e66e0036e9772b772f769e25c784c60
epezent <epezent16@gmail.com>
2023-08-19 21:08:50 -0500

    add comment about v_min/v_max to SetupAxisLimits

--- implot.h
+++ implot.h
@@ -728,7 +728,7 @@ IMPLOT_API void EndSubplots();
 
 // Enables an axis or sets the label and/or flags for an existing axis. Leave #label = nullptr for no label.
 IMPLOT_API void SetupAxis(ImAxis axis, const char* label=nullptr, ImPlotAxisFlags flags=0);
-// Sets an axis range limits. If ImPlotCond_Always is used, the axes limits will be locked.
+// Sets an axis range limits. If ImPlotCond_Always is used, the axes limits will be locked. Inversion with v_min > v_max is not supported; use SetupAxisLimits instead.
 IMPLOT_API void SetupAxisLimits(ImAxis axis, double v_min, double v_max, ImPlotCond cond = ImPlotCond_Once);
 // Links an axis range limits to external values. Set to nullptr for no linkage. The pointer data must remain valid until EndPlot.
 IMPLOT_API void SetupAxisLinks(ImAxis axis, double* link_min, double* link_max);
